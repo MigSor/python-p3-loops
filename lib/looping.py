54b38ad15ffc16319755768df6c1d8d102bace97
Miguel Soriano <miguel.sor.88@gmail.com>
2024-06-21 23:58:09 +1000

    learned to use while and for loop in Python

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    for count in range(1, 11):
+        print(count)
+    print("Happy New Year!")
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [squared_num ** 2 for squared_num in int_list]
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
